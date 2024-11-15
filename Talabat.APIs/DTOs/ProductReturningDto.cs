bc750f02062323983441011b1687b63b6f761dc3
Mohammed Hany <76411941+mohammedhany990@users.noreply.github.com>
2024-11-15 14:21:14 +0200

    Update ProductReturningDto.cs

--- Talabat.APIs/DTOs/ProductReturningDto.cs
+++ Talabat.APIs/DTOs/ProductReturningDto.cs
@@ -2,6 +2,8 @@
 {
     public class ProductReturningDto
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public string PictureUrl { get; set; }
